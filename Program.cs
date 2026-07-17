08ee2f16a7e012ac0bbac7fff4da2ce225ee435f
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2026-07-17 17:33:07 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var quantidadeInstancias = 1;
+        var quantidadeInstancias = 2;
         var processos = new List<Process>();
 
         var executionPath = AppContext.BaseDirectory;
